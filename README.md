14a5c0ab3efd336d383ad994f1546776f47f048f
Rick F <rfernandez@lightspeedsystems.com>
2014-12-03 14:13:32 -0800

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+Just a simple Message encryptor app. Basically it displays 2 links on the home page. One is to a Message Encryptor. At the message encryptor Post, I have it render a nother page that displays the message plus a "Decrypt?" button. 
+
+
+<p> Current Bugs</p>
+<p>Unable to get Decrypt Button to grab :secret key</p>
---
+++ basics.rb
@@ -0,0 +1,54 @@
+require 'rubygems'
+require 'sinatra'
+
+get '/' do
+	erb :home
+end
+
+get '/aboutme' do
+	"Just a place holder til I add an aboutme.erb"
+end
+
+
+get '/secret' do
+	erb :secret
+end
+
+post '/secret' do
+	@secret=params["secret"]
+
+	erb :decrypt
+
+end
+
+get '/decrypt/secret' do
+	
+	erb :decryptsecret
+#	params[:secret].reverse
+end
+
+
+
+
+
+
+
+
+
+
+
+# get '/hello/:name/:city' do
+# 	"Hello there, #{params[:name]} from #{params[:city].upcase}."
+# end
+
+# get '/more/*' do
+# 	params[:splat]
+# end
+
+# get '/form' do
+# 	erb :form
+# end
+
+# post '/form' do
+# 	"You said '#{params[:words]}'!"
+# end
